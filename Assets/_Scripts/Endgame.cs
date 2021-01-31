c5be9194e9db3d67cb572ae735fa5d438b07fb7d
John <john.wang@uoit.net>
2021-01-31 14:21:04 -0500

    added endgame credits

---
+++ Assets/_Scripts/Endgame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Endgame : MonoBehaviour
+{
+    Body body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.GetComponent<Body>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (body.attached) {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}
